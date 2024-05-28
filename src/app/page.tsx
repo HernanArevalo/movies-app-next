677a7b4e0f05386153583d2e926f18b576489244
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-05-28 17:39:46 -0300

    movie search page to the main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,53 @@
-'use client'
-import { useRouter } from "next/navigation";
+"use client"
 
-export default function Home() {
+import { getMovies } from "@/actions";
+import { MovieItem } from "@/components";
+import { MovieResume } from "@/interfaces";
+import { useState } from "react";
+import { IoCloseCircleSharp } from "react-icons/io5";
 
-  const router = useRouter()
-  router.replace('/search')
 
-  return;
+export default function SearchPage() {
+
+  const [input, setInput] = useState("harry potter")
+  const [movies, setMovies] = useState<MovieResume[]>([])
+
+
+  const onInputChange = async(e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+    const { movies } = await getMovies(e.target.value)
+    setMovies(movies)
+
+  }
+
+  const onInputDelete = () =>{
+    setInput('')
+  }
+
+
+  
+
+  return (
+    <>
+      <div className="search-background">
+        
+      </div>
+      <div className="p-10 flex flex-col gap-4 justify-center w-full z-10 search-content">
+        <h1 className="text-5xl">Search a Movie</h1>
+        <div className="flex flex-row gap-4 items-center">
+          <input value={input}  onChange={onInputChange} type="text" className="py-2 px-4 rounded-3xl bg-white w-96 text-black"/>
+          <IoCloseCircleSharp size={44} onClick={onInputDelete} className="cursor-pointer"/>
+        </div>
+        <div className="flex-row flex flex-wrap gap-4 justify-center">
+          { movies.map((movie, idx) =>(
+            <MovieItem key={`${idx}${movie.id}${movie.title}`} 
+                       movie={movie}
+            />
+          ))
+
+          }
+        </div>
+      </div>
+    </>
+  );
 }
