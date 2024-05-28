9f8bc184f0476b6bb7f86f98c5e79a093b5cf92a
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-05-28 17:43:55 -0300

    add Trailer Component

---
+++ src/components/TrailerComponent.jsx
@@ -0,0 +1,27 @@
+import './TrailerComponent.css';
+
+export const TrailerComponent = ( {trailer, id} ) => {
+
+  // const navigate = useNavigate()
+
+  // const goBack = () => {
+  //   navigate(`../movie/${id}`, { replace: true })
+  // }
+
+  return (
+      <section  onClick={ goBack }
+                className='trailer-container animate__animated animate__fadeInRight' >
+        <iframe 
+                className='animate__animated animate__fadeInRight'
+                src={ trailer }
+                title="YouTube video player" 
+                frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowFullScreen>
+                
+        </iframe>
+      </section>
+
+
+  )
+}
