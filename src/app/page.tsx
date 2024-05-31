926a830eb55b47dd7ecdd206abaf43b44085591d
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-05-31 13:56:23 -0300

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { IoCloseCircleSharp } from "react-icons/io5";
 
 export default function SearchPage() {
 
-  const [input, setInput] = useState("harry potter")
+  const [input, setInput] = useState("")
   const [movies, setMovies] = useState<MovieResume[]>([])
 
 
@@ -32,9 +32,9 @@ export default function SearchPage() {
       <div className="search-background">
         
       </div>
-      <div className="p-10 flex flex-col gap-4 justify-center w-full z-10 search-content">
-        <h1 className="text-5xl">Search a Movie</h1>
-        <div className="flex flex-row gap-4 items-center">
+      <div className="p-10 flex flex-col gap-2 md:gap-4 justify-center w-full z-10 search-content">
+        <h1 className="text-2xl md:text-5xl animate__animated animate__fadeInLeft">Search a Movie</h1>
+        <div className="flex flex-row gap-4 items-center animate__animated animate__fadeInLeft">
           <input value={input}  onChange={onInputChange} type="text" className="py-2 px-4 rounded-3xl bg-white w-96 text-black"/>
           <IoCloseCircleSharp size={44} onClick={onInputDelete} className="cursor-pointer"/>
         </div>
