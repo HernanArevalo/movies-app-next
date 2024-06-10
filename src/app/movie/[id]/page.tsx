23711485513a451920d14cd6084757d01fd352e1
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-06-10 20:13:23 -0300

    Not use Link in provider items

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -56,6 +56,7 @@ export default async function MoviePage({ params }: Props) {
                       alt=""
                       width={48}
                       height={48}
+                        key={provider.logo_path}
                     />
                     // </Link>
                   ))}
