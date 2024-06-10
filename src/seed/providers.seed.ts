cfcd5e2456327262464c804c02803bd872eb24ef
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-06-10 20:13:06 -0300

    providers seed

---
+++ src/seed/providers.seed.ts
@@ -0,0 +1,24 @@
+
+export const providers = [
+  { name: 'Netflix',
+    url: 'https://www.netflix.com'
+  },
+  { name: 'Amazon Prime Video',
+    url: 'https://www.primevideo.com'
+  },
+  { name: 'MovistarTV',
+    url: 'https://www.movistarplus.es/'
+  },
+  { name: 'Max',
+    url: 'https://play.max.com/'
+  },
+  { name: 'Disney Plus',
+    url: 'https://www.disneyplus.com/'
+  },
+  { name: 'Star Plus',
+    url: 'https://www.starplus.com'
+  },
+  { name: 'Paramount Plus',
+    url: 'https://www.paramountplus.com/'
+  },
+]
