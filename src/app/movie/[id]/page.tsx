8b6e68119f7536fa2f7d29dde4455d5ff58a90d5
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-06-24 17:03:27 -0300

    format document

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { IoSearch } from 'react-icons/io5';
+import { TbMovieOff } from 'react-icons/tb';
 
 import { getMovieById } from '@/actions';
-import { TrailerButton, TrailerComponent } from '@/components';
+import { TrailerButton, TrailerComponent } from './components';
 import { scoreToRgb } from '@/utils';
 
 import './page.css';
-import { TbMovieOff } from 'react-icons/tb';
 
 interface Props {
   params: {
@@ -31,12 +31,12 @@ export default async function MoviePage({ params }: Props) {
   return (
     <>
       <div
-        className="movie-container animate__animated animate__fadeIn transition-all"
+        className="movie-container animate__animated animate__fadeIn"
         style={{ backgroundImage: `url(${movie?.backdrop})` }}
       >
-        <div className="main-container  animate__animated animate__fadeIn">
+        <div className="main-container animate__animated animate__fadeRight">
           <div className="upper-container">
-            <div className="title animate__animated animate__fadeInRight flex justify-center items-end gap-0 flex-col">
+            <div className="title flex justify-center items-end gap-0 flex-col">
               <h3 className="font-extrabold">{movie?.title.toUpperCase()}</h3>
               <span
                 className="w-9 h-9 text-lg flex justify-center items-center rounded text-black"
@@ -46,30 +46,35 @@ export default async function MoviePage({ params }: Props) {
               </span>
             </div>
 
-            <div className="info-container animate__animated animate__fadeInRight">
-              <div className="year-genders animate__animated animate__fadeInRight flex flex-col gap-5 items-end">
+            <div className="info-container">
+              <div className="year-genders flex flex-col gap-5 items-end">
                 <div className="flex flex-row gap-2 items-center">
-                  {movie?.providers.length > 0 ? 
-                  movie?.providers.map((provider: any) => (
-                    // <Link key={provider.logo_path} href={''}>
+                  {movie?.providers.length > 0 ? (
+                    movie?.providers.map((provider: any) => (
+                      // <Link key={provider.logo_path} href={''}>
                       <Image
-                      src={`${provider.url}`}
-                      alt=""
-                      width={48}
-                      height={48}
-                      key={provider.logo_path}
-                      />))
+                        src={`${provider.url}`}
+                        alt=""
+                        width={48}
+                        height={48}
+                        key={provider.logo_path}
+                      />
+                    ))
+                  ) : (
                     // </Link>
-                    :
                     <div className="bg-gray-600 p-2 rounded">
-                      <TbMovieOff color='white'/>
+                      <TbMovieOff color="white" />
                     </div>
-                  }
+                  )}
                 </div>
                 <div className="flex justify-center items-center gap-2">
-                  <span className=" flex justify-center items-center">{movie?.year}</span>
+                  <span className=" flex justify-center items-center">
+                    {movie?.year}
+                  </span>
                   <span className=" flex justify-center items-center">|</span>
-                  <span className="font-normal flex justify-center items-center">{movie?.genres.join(", ")}</span> 
+                  <span className="font-normal flex justify-center items-center">
+                    {movie?.genres.join(', ')}
+                  </span>
                 </div>
               </div>
               <div className="story-title flex flex-row gap-5 justify-end pr-5">
