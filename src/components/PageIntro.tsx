056e9a52513366ce207949185866d2fcdea355f0
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-06-24 17:56:59 -0300

    change text

--- src/components/PageIntro.tsx
+++ src/components/PageIntro.tsx
@@ -24,7 +24,7 @@ export const PageIntro = () => {
     <div>
       {isLoading && firstTime && (
         <div className="fixed inset-0 flex items-center justify-center bg-black z-20  animate__animated animate__fadeOut animate__delay-3s">
-          <h1 className="text-white text-4xl animate__animated animate__fadeIn animate__delay-1s">Movies App</h1>
+          <h1 className="text-white text-4xl animate__animated animate__fadeIn animate__delay-1s">Movies App.</h1>
         </div>
       )}
     </div>
