88143e54c5d7ea79e0f6692f59b5bf2d8cf57c96
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-06-24 16:58:41 -0300

    create (search) folder

---
+++ src/app/(search)/page.tsx
@@ -0,0 +1,22 @@
+import { PageIntro } from '@/components';
+
+import { Input, MoviesGrid } from './components';
+
+export default function SearchPage() {
+
+  return (
+    <>
+      <PageIntro />
+      <div className="search-background" />
+
+      <div className="p-10 flex flex-col gap-2 md:gap-4 w-full z-10 search-content">
+        <h1 className="text-2xl md:text-5xl animate__animated animate__fadeInLeft w-fit">
+          Search a Movie
+        </h1>
+        <Input />  
+
+        <MoviesGrid />
+      </div>
+    </>
+  );
+}
