1b8bc706cc3728a72b6dbf2830e3f45206a3622f
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-06-28 21:20:31 -0300

    formato document

--- src/components/PageIntro.tsx
+++ src/components/PageIntro.tsx
@@ -1,10 +1,9 @@
-"use client"
+'use client';
 import { MoviesStore } from '@/app/store';
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 
 export const PageIntro = () => {
-  const { firstTime, cancelFirstTime } = MoviesStore()
-  
+  const { firstTime, cancelFirstTime } = MoviesStore();
 
   useEffect(() => {
     if (firstTime) {
@@ -13,16 +12,18 @@ export const PageIntro = () => {
       }, 5000);
 
       return () => clearTimeout(timer);
-    } 
+    }
   }, [firstTime, cancelFirstTime]);
 
   return (
     <div>
-      { firstTime && (
+      {firstTime && (
         <div className="fixed inset-0 flex items-center justify-center bg-black z-20 pointer-events-none animate__animated animate__fadeOut animate__delay-3s">
-          <h1 className="text-white text-4xl animate__animated animate__fadeIn animate__delay-1s">Movies App.</h1>
+          <h1 className="text-white text-4xl animate__animated animate__fadeIn animate__delay-1s">
+            Movies App.
+          </h1>
         </div>
       )}
     </div>
   );
-}
+};
